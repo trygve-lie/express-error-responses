3e63fcca10559c575d82911466eb0de3817baae2
Trygve Lie <github@trygve-lie.com>
2015-06-24 13:54:29 +0200

    Added test

--- test/error.js
+++ test/error.js
@@ -4,3 +4,13 @@
 
 var tap     = require('tap'),
     error   = require('../lib/error.js');
+
+
+
+tap.test('error.error() - no parameters provided', function (t) {
+    var result = error.error();
+    t.type(result, 'Error');
+    t.equal(result.type, 'GenericError');
+    t.notOk(result.message);  // should be null
+	t.end();
+});
