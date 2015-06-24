a1ee4d1ec14f3c6c9e3d590becdf68c6f7069aae
Trygve Lie <github@trygve-lie.com>
2015-06-24 12:09:37 +0200

    Initial commit

---
+++ test/error.js
@@ -0,0 +1,6 @@
+/* jshint node: true, strict: true */
+
+"use strict";
+
+var tap     = require('tap'),
+    error   = require('../lib/error.js');
---
+++ test/middleware.js
@@ -0,0 +1,6 @@
+/* jshint node: true, strict: true */
+
+"use strict";
+
+var tap         = require('tap'),
+    middleware  = require('../lib/middleware.js');
